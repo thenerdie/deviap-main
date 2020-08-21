7bb1c808554e6dd166118f5cb502dd0432ba0572
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-08-21 18:50:52 -0400

    Legacy entry points (0271 & below)

---
+++ core/dashboard/main.lua
@@ -0,0 +1,6 @@
+-- Copyright 2020 - Deviap (deviap.com)
+-- Author(s): Sanjay-B(Sanjay)
+
+-- Points to (../source/application/main.lua) is still enforced so, we're forced to use it. 
+
+require("tevgit:source/application/main.lua")
---
+++ core/teverseUI/main.lua
@@ -0,0 +1,6 @@
+-- Copyright 2020 - Deviap (deviap.com)
+-- Author(s): Sanjay-B(Sanjay)
+
+-- Points to (../source/application/main.lua) is still enforced so, we're forced to use it when we reload the entire application. 
+
+require("tevgit:source/application/main.lua")
