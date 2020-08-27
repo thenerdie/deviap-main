daf726c2c0f917611ed01519adf6655bfe11afb4
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-08-27 10:41:31 -0400

    UI lib entry point

---
+++ source/libraries/UI/main.lua
@@ -0,0 +1,10 @@
+-- Copyright 2020 - Deviap (deviap.com)
+-- Author(s): Sanjay-B(Sanjay)
+
+-- Entry point for UI library.
+
+-- Quick Imports
+return {
+    -- class = require("../path")
+    -- Classes are camelCase.
+}
