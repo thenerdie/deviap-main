1b05486f71d7a6c64f5ab7535e78d60f0ee9cec0
u-train <skassem05@outlook.com>
2020-08-28 11:15:28 -0400

    Added outline for the module.

--- source/libraries/UI/constraints.lua
+++ source/libraries/UI/constraints.lua
@@ -1,5 +1,128 @@
 -- Copyright 2020 - Deviap (deviap.com)
--- Author(s): YOUR NAME, OTHER PERSON'S NAME
+-- Author(s): utrain, Ya
 
--- REPLACE WITH ONE LINE DESCRIPTION OF WHAT THIS FILE DOES
-return {}
+-- Constraints using tabs. Some components of this are not implemented. This is not is also not the final interface.
+
+local new = function()
+	local public = {}
+	public.tabs = { x = {}; y = {} }
+	public.anchors = { x = {}; y = {} } -- ez reference
+
+	public.registerRelativeTab = function(axis, tag, relativeTo, min, max, pref, weight)
+		--[[
+			@description
+				Registers a new relative tab.
+
+			@parameters
+				string, axis
+					Axis of which the new axis will be placed.
+				string, tag
+					The tag of the tab. Cannot conflict with already existing tags. Used to reference the tab.
+				integer, min
+					Minimum distance away from a tab it's set relative to.
+				integer, [max]
+					Maximum distance away from a tab it's relative to. Defaults to min if not defined.
+				integer, [pref]
+					Preferred distance away from a tab it's relative to. Defaults to min if not defined.
+				integer, [weight]
+					How much weight is given to this tab (DEPENDS ON RESOLVER USED). Defaults to 1 if not defined.
+
+			@returns
+				nil
+		]]
+	end
+
+	public.registerAnchorTab = function(axis, tag, offset)
+		--[[
+			@description
+				Register an anchor tabs
+
+			@parameters
+				string, axis
+					Axis of which the new tab will be placed.
+				string, tag
+					The tag of the tab. Cannot conflict with already existing tags. Used to reference the tab.
+				integer, offset
+					The offset away from the border.
+
+			@returns
+				nil
+		]]
+	end
+
+	public.pokeTab = function(axis, tag)
+		--[[
+			@description
+				pokey tab haha
+
+			@parameters
+				string, axis
+					Axis of the tab you wanna poke.
+				string, tag
+					Tag of the tab you wanna poke.
+
+			@returns
+				tab, tab you wanna poke
+		]]
+	end
+
+	public.removeTab = function(axis, tag)
+		--[[
+			@description
+				UNSAFE: removes tab and returns the tab(s) that was relative to it.
+
+			@parameters
+				string, axis
+					Axis of tab to remove.
+				string, tag
+					Tag of the tab to remove.
+
+			@returns
+				tab, tab that was relative to the tab removed.
+		]]
+	end
+
+	public.isSafeToRemove = function(axis, tag)
+		--[[
+			@description
+				UNSAFE: removes tab and returns the tab(s) that was relative to it.
+
+			@parameters
+				string, axis
+					Axis of tab to remove.
+				string, tag
+					Tag of the tab to remove.
+
+			@returns
+				tab, tab that was relative to the tab removed.
+		]]
+	end
+
+	public.resolveForAxis = function(axis, maxLength)
+		--[[
+			@description
+				Resolves for the given axis.
+
+			@parameters
+				string, axis
+					Axis to resolve.
+			@returns
+				hashmap<string, integer>
+					with string being tag of tab and integer being the position.
+		]]
+	end
+
+	public.resolve = function(maxSize)
+			--[[
+			@description
+				Returns the resolved positions of all of the tabs.
+
+			@returns
+				{ x = resolveForAxis("x"); y = resolveForAxis("y") }
+		]]
+	end
+
+	return public
+end
+
+return { new = new }
