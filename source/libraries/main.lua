be70b67ab839c95b80784be3908c83f7c2ca534c
utrain <63367489+u-train@users.noreply.github.com>
2020-08-28 21:28:39 -0400

    Enterprise code: final fix
    You can blame Sanjay. He's my god now.

--- source/libraries/main.lua
+++ source/libraries/main.lua
@@ -4,21 +4,24 @@
 -- This file lazy-loads our modules that we have made. Enterprise grade, of course.
 
 local globalStringSubstition = string.gsub
-local rootDirectory = "tevgit" -- Must ensure our code is enterprise-grade by ensuring modularity.
-local stringReplacementForPeriod = ("%:source/libraries/"):gsub("%%", rootDirectory)
+local rootDirectory = "scripts/" -- Must ensure our code is enterprise-grade by ensuring modularity.
+local stringReplacementForPeriod = ("%source/libraries/"):gsub("%%", rootDirectory)
 local errorMessageIfModuleFailsToLoad = "You either misnamed the library or you forgot './' like a functional programmer."
 local regex = "^%."
-local requireModule = function(moduleName)
-	return pcall(require(moduleName)) or nil
+
+local errorIfModuleFailsToLoad = function(msg)
+	error(msg, 4)
 end
-local errorIfModuleFailsToLoad = function(errorMessage)
-	error(errorMessage, 3)
+local requireModule = function(moduleName)
+	local success, returnVal = pcall(require, moduleName)
+	if not success then errorIfModuleFailsToLoad(errorMessageIfModuleFailsToLoad) end
+	return returnVal
 end
 
 local librariesRelativePath =
 {
-	--libraryName = "./..."
-	UI = "./UI/main.lua"
+	-- libraryName = "./..."
+	UI = "./b.lua";
 }
 
-return function(libraryName) return requireModule(globalStringSubstition(librariesRelativePath[libraryName], regex, stringReplacementForPeriod)) or errorIfModuleFailsToLoad(errorMessageIfModuleFailsToLoad) end
+return function(libraryName) return requireModule(globalStringSubstition(librariesRelativePath[libraryName], regex, stringReplacementForPeriod)) end
