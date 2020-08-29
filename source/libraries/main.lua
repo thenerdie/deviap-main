e5d61f533ae3f52917aad7ea3d40523e5891e2e6
utrain <63367489+u-train@users.noreply.github.com>
2020-08-28 20:46:09 -0400

    Enterprise grade code guys.
    This is for Sanjay when he said to change the name of the variable from libsLocation to libsPath.

--- source/libraries/main.lua
+++ source/libraries/main.lua
@@ -1,9 +1,24 @@
-local libsLocations =
+-- Copyright 2020 - Deviap (deviap.com)
+-- Author(s): utrain
+
+-- This file lazy-loads our modules that we have made. Enterprise grade, of course.
+
+local globalStringSubstition = string.gsub
+local stringReplacementForPeriod = "tevgit:source/libraries/"
+local errorMessageIfModuleFailsToLoad = "You either misnamed the library or you forgot './' like a functional programmer."
+local regex = "^%."
+
+local requireModule = function(moduleName)
+	return require(moduleName)	
+end
+local errorIfModuleFailsToLoad = function(msg)
+	error(msg, 3)
+end
+
+local librariesRelativePath =
 {
-	--libName = "./..."
+	--libraryName = "./..."
 	UI = "./UI/main.lua"
 }
 
-return function(libName)
-	return require(libsLocations[libName]:gsub("^", "tevgit:source/libraries/"))
-end
+return function(libraryName) return require(librariesRelativePath[libraryName]:gsub(regex, stringReplacementForPeriod)) or errorIfModuleFailsToLoad(errorMessageIfModuleFailsToLoad) end
