1298ec79450696f5feb70b3efedb23fec9566eaf
utrain <63367489+u-train@users.noreply.github.com>
2020-08-28 21:30:22 -0400

    Enterprise mistakes causes enterprise fixes.
    Enterpise solution: fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, commit, fix, and commit. Then the PR.

--- source/libraries/main.lua
+++ source/libraries/main.lua
@@ -4,7 +4,7 @@
 -- This file lazy-loads our modules that we have made. Enterprise grade, of course.
 
 local globalStringSubstition = string.gsub
-local rootDirectory = "scripts/" -- Must ensure our code is enterprise-grade by ensuring modularity.
+local rootDirectory = "tevgit:" -- Must ensure our code is enterprise-grade by ensuring modularity.
 local stringReplacementForPeriod = ("%source/libraries/"):gsub("%%", rootDirectory)
 local errorMessageIfModuleFailsToLoad = "You either misnamed the library or you forgot './' like a functional programmer."
 local regex = "^%."
@@ -21,7 +21,7 @@ end
 local librariesRelativePath =
 {
 	-- libraryName = "./..."
-	UI = "./b.lua";
+	UI = "./UI/main.lua";
 }
 
 return function(libraryName) return requireModule(globalStringSubstition(librariesRelativePath[libraryName], regex, stringReplacementForPeriod)) end
