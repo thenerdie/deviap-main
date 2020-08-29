1de66e5a3fce29c6b2d36ae3ebd7086abc0ae1c9
REGENER ATE <stirmanjr@gmail.coom>
2020-08-29 16:45:26 -0400

    Added code and description for enum.lua

--- source/libraries/UI/enum.lua
+++ source/libraries/UI/enum.lua
@@ -1,5 +1,45 @@
 -- Copyright 2020 - Deviap (deviap.com)
--- Author(s): YOUR NAME, OTHER PERSON'S NAME
+-- Author(s): KISPERAL
 
--- REPLACE WITH ONE LINE DESCRIPTION OF WHAT THIS FILE DOES
-return {}
+-- PROVIDES A SINGLETON CLASS TO CREATE ENUMS, AND RESOLVE THOSE ENUMS
+
+local lastValue = 0
+
+local enumerable = {}
+
+enumerable.tree = {}
+
+local function recursiveIterate(table, callback)
+    for i, v in pairs(table) do
+        if type(v) == "table" then 
+            recursiveIterate(v, callback)
+        else
+            callback(i, v)
+        end
+    end
+end
+
+function enumerable:newEnum(directory, name, value)
+    local curDirectory = self.tree
+
+    for _, dirName in pairs(string.split(directory, "/")) do
+        if curDirectory[dirName] == nil then
+            curDirectory[dirName] = {}
+        end
+        curDirectory = curDirectory[dirName]
+    end
+    curDirectory[name] = value or (lastValue+1)
+end
+
+function enumerable:resolveValue(value)
+    local t = type(value)
+    local ret = nil
+    recursiveIterate(value, function(index, obj)
+        if (t == "string") and (index == value) or (obj == value) then
+            ret = (t == "string") and index or obj
+        end
+    end)
+    return ret
+end
+
+return enumerable
